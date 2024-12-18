4d81fd1715df8935697481094f7380c1c1632da9
JB Paul <3.ria.eboda@gmail.com>
2024-12-18 20:57:47 +0900

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,51 @@
+import { LoadSkiaWeb } from "@shopify/react-native-skia/lib/module/web";
+import { useEffect, useState } from "react";
+import {
+  ActivityIndicator,
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+import { Main } from "./src/Main";
+
+export default function App() {
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    if (Platform.OS === "web") {
+      LoadSkiaWeb({ locateFile: () => "/canvaskit.wasm" })
+        .then(() => {
+          setLoaded(true);
+        })
+        .catch((err) => console.error(err));
+    } else {
+      setLoaded(true);
+    }
+  }, []);
+
+  if (!loaded) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>Loading Skia...</Text>
+        <ActivityIndicator style={styles.indicator} />
+      </View>
+    );
+  }
+
+  return <Main />;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: 20,
+  },
+  indicator: {
+    marginTop: 20,
+  },
+});
---
+++ src/Main.tsx
@@ -0,0 +1,11 @@
+import { SafeAreaView, Text } from "react-native";
+
+const Main = () => {
+  return (
+    <SafeAreaView>
+      <Text>Hello world</Text>
+    </SafeAreaView>
+  );
+};
+
+export { Main };
